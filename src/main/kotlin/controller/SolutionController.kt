58a6cd90c464cfddeeab3594a434a6f4a673e9ee
Wicar Pessoa <wicarpp@gmail.com>
2025-05-30 00:09:55 -0300

    refactor: remove duplicate CORS configuration from SolutionController

--- src/main/kotlin/controller/SolutionController.kt
+++ src/main/kotlin/controller/SolutionController.kt
@@ -19,7 +19,6 @@ class SolutionController : HttpHandler {
         try {
             when (exchange.requestMethod) {
                 "GET" -> handleGetAllSolutions(exchange)
-                "OPTIONS" -> handleCorsOptions(exchange)
                 else -> {
                     exchange.sendResponseHeaders(405, -1)
                 }
@@ -30,20 +29,7 @@ class SolutionController : HttpHandler {
         }
     }
 
-    // Tratamento para CORS quando o frontend estiver em um domínio diferente
-    private fun handleCorsOptions(exchange: HttpExchange) {
-        setCorsHeaders(exchange)
-        exchange.sendResponseHeaders(204, -1)
-    }
-    
-    private fun setCorsHeaders(exchange: HttpExchange) {
-        exchange.responseHeaders.add("Access-Control-Allow-Origin", "https://clark-wright-algorithm-front-ljfkge6ds-wicar-pessoas-projects.vercel.app")
-        exchange.responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        exchange.responseHeaders.add("Access-Control-Allow-Headers", "Content-Type")
-    }
-
     private fun respond(exchange: HttpExchange, statusCode: Int, response: String) {
-        setCorsHeaders(exchange)
         exchange.responseHeaders.add("Content-Type", "application/json")
         exchange.sendResponseHeaders(statusCode, response.toByteArray().size.toLong())
         exchange.responseBody.use { os -> os.write(response.toByteArray()) }
