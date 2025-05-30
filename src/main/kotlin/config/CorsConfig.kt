ef6d6d81f6a7844419c76b90b48b2e60ef75c3ef
Wicar Pessoa <wicarpp@gmail.com>
2025-05-29 23:56:14 -0300

    fix: configure CORS to allow only 34.205.72.237

--- src/main/kotlin/config/CorsConfig.kt
+++ src/main/kotlin/config/CorsConfig.kt
@@ -5,21 +5,17 @@ import com.sun.net.httpserver.HttpHandler
 
 class CorsHandler(private val handler: HttpHandler) : HttpHandler {
     override fun handle(exchange: HttpExchange) {
-        val origin = exchange.requestHeaders.getFirst("Origin")
         val method = exchange.requestMethod
         println("DEBUG - Request details:")
-        println("Origin: $origin")
         println("Method: $method")
         println("URI: ${exchange.requestURI}")
         println("Headers: ${exchange.requestHeaders}")
-        
+
         // Set CORS headers
         exchange.responseHeaders.add("Access-Control-Allow-Origin", "http://34.205.72.237")
-        exchange.responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-        exchange.responseHeaders.add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+        exchange.responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        exchange.responseHeaders.add("Access-Control-Allow-Headers", "Content-Type")
         exchange.responseHeaders.add("Access-Control-Allow-Credentials", "true")
-        exchange.responseHeaders.add("Access-Control-Max-Age", "86400")
-        exchange.responseHeaders.add("Access-Control-Expose-Headers", "*")
 
         // Handle preflight requests
         if (method == "OPTIONS") {
