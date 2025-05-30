afcb471c125c4fcae8be9d5f6e11e16751ab4d7d
Wicar Pessoa <wicarpp@gmail.com>
2025-05-30 00:11:27 -0300

    refactor: remove duplicate CORS configuration from SolveController and StrategyController

--- src/main/kotlin/controller/SolveController.kt
+++ src/main/kotlin/controller/SolveController.kt
@@ -170,7 +170,6 @@ class SolveController : HttpHandler {
             when (exchange.requestMethod) {
                 "POST" -> handleSolveRequest(exchange)
                 "GET" -> handleGetSolutions(exchange)
-                "OPTIONS" -> handleCorsOptions(exchange)
                 else -> {
                     exchange.sendResponseHeaders(405, -1)
                 }
@@ -180,21 +179,8 @@ class SolveController : HttpHandler {
             respond(exchange, 500, """{"error": "${e.message}"}""")
         }
     }
-    
-    // Tratamento para CORS quando o frontend estiver em um domínio diferente
-    private fun handleCorsOptions(exchange: HttpExchange) {
-        setCorsHeaders(exchange)
-        exchange.sendResponseHeaders(204, -1)
-    }
-    
-    private fun setCorsHeaders(exchange: HttpExchange) {
-        exchange.responseHeaders.add("Access-Control-Allow-Origin", "https://clark-wright-algorithm-front-ljfkge6ds-wicar-pessoas-projects.vercel.app")
-        exchange.responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        exchange.responseHeaders.add("Access-Control-Allow-Headers", "Content-Type")
-    }
 
     private fun respond(exchange: HttpExchange, statusCode: Int, response: String) {
-        setCorsHeaders(exchange)
         exchange.responseHeaders.add("Content-Type", "application/json")
         exchange.sendResponseHeaders(statusCode, response.toByteArray().size.toLong())
         exchange.responseBody.use { os -> os.write(response.toByteArray()) }
--- src/main/kotlin/controller/StrategyController.kt
+++ src/main/kotlin/controller/StrategyController.kt
@@ -25,7 +25,6 @@ class StrategyController : HttpHandler {
         try {
             when (exchange.requestMethod) {
                 "GET" -> handleGetStrategies(exchange)
-                "OPTIONS" -> handleCorsOptions(exchange)
                 else -> {
                     exchange.sendResponseHeaders(405, -1)
                 }
@@ -35,21 +34,8 @@ class StrategyController : HttpHandler {
             respond(exchange, 500, """{"error": "${e.message}"}""")
         }
     }
-    
-    // Tratamento para CORS quando o frontend estiver em um domínio diferente
-    private fun handleCorsOptions(exchange: HttpExchange) {
-        setCorsHeaders(exchange)
-        exchange.sendResponseHeaders(204, -1)
-    }
-    
-    private fun setCorsHeaders(exchange: HttpExchange) {
-        exchange.responseHeaders.add("Access-Control-Allow-Origin", "https://clark-wright-algorithm-front-ljfkge6ds-wicar-pessoas-projects.vercel.app")
-        exchange.responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        exchange.responseHeaders.add("Access-Control-Allow-Headers", "Content-Type")
-    }
 
     private fun respond(exchange: HttpExchange, statusCode: Int, response: String) {
-        setCorsHeaders(exchange)
         exchange.responseHeaders.add("Content-Type", "application/json")
         exchange.sendResponseHeaders(statusCode, response.toByteArray().size.toLong())
         exchange.responseBody.use { os -> os.write(response.toByteArray()) }
